589ab035f26b6650a27d11fc784700f534aa75d2
AbdulRehman <likeam99@gmail.com>
2023-01-23 00:36:26 +0500

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [msg, setMsg] = useState('');
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetch('https://jsonplaceholder.typicode.com/posts').then(response => response.json());
+      setMsg(data);
+    }
+    fetchData();
+  }, [])
+
+  return <div className="App">
+    <h1>Counter1:{msg}</h1>
+  </div>
 }
 
 export default App;
